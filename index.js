2d225b9e4ab0b3f1bb7ff5a70d0d9913cba52093
Baptistekeunbroek <baptiste.keunebroek@gmail.com>
2024-12-10 20:00:02 +0100

    Fix text

--- index.js
+++ index.js
@@ -19,13 +19,19 @@ const client = new Client({
 const adminRoleName = "Discord Admin"; // Adjust this to match your admin role name
 const BotRoleName = "BOTS"
 const welcomeMessage = `
-Welcome on the server **{server}**! Please share a screenshot of your game showing your name to make sure you are not a spy!
-**Français:** Bienvenue sur le serveur **{server}** ! Veuillez partager une capture d'écran de votre jeu montrant votre nom pour vérifier que vous n'êtes pas un espion !
-**Deutsch:** Willkommen auf dem Server **{server}**! Bitte teile einen Screenshot deines Spiels, der deinen Namen zeigt, um sicherzustellen, dass du kein Spion bist!
-**Русский:** Добро пожаловать на сервер **{server}**! Пожалуйста, отправьте скриншот вашей игры, на котором видно ваше имя, чтобы убедиться, что вы не шпион!
-**Español:** ¡Bienvenido al servidor **{server}**! ¡Por favor comparte una captura de pantalla de tu juego mostrando tu nombre para asegurarte de que no eres un espía!
-**中文:** 歡迎來到伺服器 **{server}**！請分享一張顯示你名字的遊戲截圖，以確保你不是間諜！
-**Italiano:** Benvenuto nel server **{server}**! Per favore, condividi uno screenshot del tuo gioco che mostra il tuo nome per essere sicuro che non sei una spia!
+**English:** Welcome on the server ! Please share a screenshot of your game showing your name to make sure you are not a spy!
+
+**Français:** Bienvenue sur le serveur ! Veuillez partager une capture d'écran de votre jeu montrant votre nom pour vérifier que vous n'êtes pas un espion !
+
+**Deutsch:** Willkommen auf dem Server ! Bitte teile einen Screenshot deines Spiels, der deinen Namen zeigt, um sicherzustellen, dass du kein Spion bist!
+
+**Русский:** Добро пожаловать на сервер ! Пожалуйста, отправьте скриншот вашей игры, на котором видно ваше имя, чтобы убедиться, что вы не шпион!
+
+**Español:** ¡Bienvenido al servidor ! ¡Por favor comparte una captura de pantalla de tu juego mostrando tu nombre para asegurarte de que no eres un espía!
+
+**中文:** 歡迎來到伺服器 ！請分享一張顯示你名字的遊戲截圖，以確保你不是間諜！
+
+**Italiano:** Benvenuto nel server ! Per favore, condividi uno screenshot del tuo gioco che mostra il tuo nome per essere sicuro che non sei una spia!
 `;
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}`);
